c1d05c9aef5cf62eba6dc96bc1bd05a8fe374835
AzulEterno <75287037+AzulEterno@users.noreply.github.com>
2024-07-06 14:08:10 +0800

    Fix build process.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Serialize build task
+
+if [ ! -d "build_Custom" ]; then
+    mkdir "build_Custom"
+fi
+
+
+if [ ! -d "build_Custom/AVX2" ]; then
+    mkdir "build_Custom/AVX2"
+fi
+
+
+
+if [ ! -d "build_Custom/AVX512" ]; then
+    mkdir "build_Custom/AVX512"
+fi
+
+
+cd "build_Custom/AVX2"
+
+../../configure MY_MPI_GCC_OMP
+make
+
+
+cd ../../
+
+cd "build_Custom/AVX512"
+
+../../configure MY_MPI_GCC_OMP
+make
